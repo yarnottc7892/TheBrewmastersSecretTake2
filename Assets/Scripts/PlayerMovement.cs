ba8daf7253da7467cb755d25a1370e6d306793c7
yarnottc7892 <yarnottc7892@my.uwstout.edu>
2020-04-18 22:08:54 -0500

    Movement and Player stuff

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHori = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(moveHori, moveVert) * speed;
+    }
+}
