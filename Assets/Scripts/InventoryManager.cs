bdafdf1eb3be3e07f9b3949dd83594a735403dde
DESKTOP-SP8RSGU\kingg <kinggold2@hotmail.com>
2020-04-20 00:56:37 -0500

    And the inventory expands

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    IngredientManager ingredientManager;
+    public List<InventorySlot> invSlot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ingredientManager = GetComponent<IngredientManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
