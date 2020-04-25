ef8092b8ae51f2cc710f2c108b8ecbb95b0516d3
ConDroid2 <47331819+ConDroid2@users.noreply.github.com>
2020-04-25 02:47:49 -0500

    Made a lot of bug fixes to the way the cards work and stuff. Wrote the code for all the effects and created a 2,3,5,8 version of all of them. Wrote the base of the code for the battle.

--- Assets/Scripts/Battle/PlayerController.cs
+++ Assets/Scripts/Battle/PlayerController.cs
@@ -5,11 +5,6 @@ using DG.Tweening;
 
 public class PlayerController : Combatant_Base
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/CardScripts/Effects/Shield.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Damage", menuName = "Cards/Effect/Shield")]
+public class Shield : Effect_Base
+{
+    public override void DoEffect(Transform target) {
+
+        target.GetComponent<Combatant_Base>().applyShield(effectValue);
+    }
+
+    public override string generateDescription() {
+
+        return "Add " + effectValue + " shield";
+    }
+}
